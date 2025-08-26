aad93b4a7508e8f1e10030c9e2709dbf2693697c
stefan muhsin <stefanmuhsin@gmail.com>
2025-08-26 12:35:15 +0900

    adding percentage and average to dashboard

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,23 @@
 class TransactionsController < ApplicationController
+  def expenses_until_same_day_last_month
+    start_date = Time.current.last_month.beginning_of_month
+
+    end_date = [
+      Time.current.last_month.end_of_month,
+      Time.current.last_month.change(day: Time.current.day)
+    ].min
+
+    where("amount > 0").where(created_at: start_date..end_date)
+  end
+
+  def expenses_until_same_day_this_month
+    start_date = Time.current.beginning_of_month
+
+    end_date = [
+    Time.current.end_of_month,
+    Time.current.change(day: Time.current.day)
+    ].min
+
+    where("amount > 0").where(created_at: start_date..end_date)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def compare_last_month
+    number_to_percentage.call((Transcation.expenses_until_same_day_last_month - Transaction.this_month_expenses) / Transcation.expenses_until_same_day_last_month)
+  end
 end
