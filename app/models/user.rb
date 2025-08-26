c225824bec029e71937f085ff23a654e4a3a8963
cdGaetan Duquesnoy <gaetan.duquesnoy@gmail.com>
2025-08-26 11:35:28 +0900

    fixed minor error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
       :recoverable, :rememberable, :validatable
-      
+
   has_many :categories
+  has_many :messages
+  has_many :transactions
 
     validates :name, presence: true
     validates :starting_balance, numericality: { only_integer: true }
