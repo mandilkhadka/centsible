b6a279b30b0c845cead86341458f55eff6d31e48
stefan muhsin <stefanmuhsin@gmail.com>
2025-08-26 16:34:53 +0900

    improved stats logic

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,12 +1,2 @@
 class TransactionsController < ApplicationController
-  def percentage_compared_to_last_month
-    this_month = Transaction.total_expenses_from_start_of_month
-    last_month = Transaction.total_expenses_until_same_day_last_month
-    spending_difference = (this_month - last_month) / last_month.to_f
-    "#{spending_difference.round(2)}%"
-  end
-
-  def daily_average
-    Transaction.total_expenses_past_30_days / 30
-  end
 end
