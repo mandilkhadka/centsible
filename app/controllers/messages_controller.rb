adcfec5a015be64de29f4174bf8019b47ab59fc4
stefan muhsin <stefanmuhsin@gmail.com>
2025-08-29 10:15:55 +0900

    simple scroll to bottom implemented

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,7 +25,6 @@ class MessagesController < ApplicationController
         )
       ]
     end
-
     format.html { redirect_to messages_path }
   end
     else
--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -1,8 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="messa
 export default class extends Controller {
   connect() {
-    this.scrollToBottom()
+    this.scrollToForm()
+    document.addEventListener("turbo:after-stream-append", this.scrollToForm)
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:after-stream-append", this.scrollToForm)
+  }
+
+  scrollToForm = () => {
+    const form = document.getElementById("chat-form")
+    if (form) {
+      form.scrollIntoView({ behavior: "smooth", block: "nearest" })
+    }
   }
 }
