088cd421faade27fd93d8a599599f34cb7d907af
mandilkhadka <khadkamandil62@gmail.com>
2025-09-02 17:18:28 +0900

    picture working but final adding to the transactions not working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,37 +5,47 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @user_message = Message.create(content: message_params[:content], role: 'user', user: current_user)
+    @user_message = Message.create(content: message_params[:content] || "Receipt", role: 'user', user: current_user, file: message_params[:file])
 
     instructions = @user_message.build_content
     build_conversation_history
     history = PastTransactionTool.new(current_user)
     categories = CategoriesTool.new(current_user)
-    transaction_maker = TextTransactionMakerTool.new(current_user, CategoriesFinderTool.new(current_user))
-    response = @ruby_llm_chat.with_tools(categories, history, transaction_maker).with_instructions(instructions).ask(@user_message.content).content
-    @ai_message = Message.create(content: response, role: 'assistant', user: current_user)
-    if @ai_message.valid?
-  respond_to do |format|
-    format.turbo_stream do
-      render turbo_stream: [
-        turbo_stream.append("messages",
-          partial: "partials/message", locals: { message: @user_message }
-        ),
-        turbo_stream.append("messages",
-          partial: "partials/message", locals: { message: @ai_message }
-        ),
-        turbo_stream.replace("chat_dashboard",
-          partial: "partials/chat_form", locals: { user_message: Message.new }
-        )
-      ]
+     if @user_message.file.attached?
+       @response= send_picture(model: "gemini-2.5-flash", with: { file: @user_message.file.url }).content
+      @ai_message = Message.create(content: @response, role: 'assistant', user: current_user)
+     end
 
-    end
-    format.html { redirect_to messages_path }
-  end
+     unless @user_message.file.attached?
+        transaction_maker = TextTransactionMakerTool.new(current_user, CategoriesFinderTool.new(current_user))
+        response = @ruby_llm_chat.with_tools(categories, history, transaction_maker).with_instructions(instructions).ask(@user_message.content).content
+        @ai_message = Message.create(content: response, role: 'assistant', user: current_user)
+     end
+
+    if @ai_message.valid?
+        respond_to do |format|
+          format.html { redirect_to messages_path }
+
+          format.turbo_stream do
+            render turbo_stream: [
+              turbo_stream.append("messages",
+                partial: "partials/message", locals: { message: @user_message }
+              ),
+              turbo_stream.append("messages",
+                partial: "partials/message", locals: { message: @ai_message }
+              ),
+              turbo_stream.replace("chat_dashboard",
+                partial: "partials/chat_form", locals: { user_message: Message.new }
+              )
+            ]
+          end
+        end
+          # format.html { redirect_to messages_path }
     else
       render :index, status: :unprocessable_entity
     end
   end
+
 
   def build_conversation_history
     @ruby_llm_chat = RubyLLM.chat(model: 'gemini-2.5-flash')
@@ -44,9 +54,17 @@ class MessagesController < ApplicationController
     end
   end
 
+  def send_picture(model: "gemini-2.5-flash", with: {})
+    instruction = "Get me the date, total and make a category for the transaction which is included in this [ 'Food', 'Health', 'Commute', 'Utilities', 'Entertainment', 'Others']. and then give it a name for the transaction."
+    @chat = RubyLLM.chat(model: model)
+    @response = @chat.with_instructions(instruction).ask(@user_message.content , with: with)
+  end
+
   private
 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :file)
   end
 end
+
+# "Get me the date, total and make a category for the transaction which is included in this [ 'Food', 'Health', 'Commute', 'Utilities', 'Entertainment', 'Others']. and then give it a name for the transaction."
